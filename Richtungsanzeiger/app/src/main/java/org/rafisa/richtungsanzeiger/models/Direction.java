adefbb2c733efee2191ae43859a115781bad609e
Jonas Tochtermann <thysia.zosa@gmail.com>
2021-03-04 19:27:27 +0100

    RIchtungsberechnung korrigiert
    und LandscapeModus ausgeschaltet

--- Richtungsanzeiger/app/src/main/java/org/rafisa/richtungsanzeiger/models/Direction.java
+++ Richtungsanzeiger/app/src/main/java/org/rafisa/richtungsanzeiger/models/Direction.java
@@ -26,13 +26,18 @@ public class Direction {
      * @return Richtung von startingLocation zu targetLocation in Grad
      */
     private int calculateDirectionAzimuth() {
-        double x = Math.cos(targetLocation.getLatitude()) * Math.sin(targetLocation.getLongitude()
-                - startingLocation.getLongitude());
-        double y = Math.cos(startingLocation.getLatitude()) * Math.sin(targetLocation.getLatitude())
-                - Math.sin(startingLocation.getLatitude()) * Math.cos(startingLocation.getLatitude())
-                * Math.cos(targetLocation.getLongitude() - startingLocation.getLongitude());
+        double startLat = Math.toRadians(startingLocation.getLatitude());
+        double startLong = Math.toRadians(startingLocation.getLongitude());
+        double targetLat = Math.toRadians(targetLocation.getLatitude());
+        double targetLong = Math.toRadians(targetLocation.getLongitude());
+        double diffLong = targetLong - startLong;
+        double x = Math.cos(targetLat) * Math.sin(diffLong);
+        double y = Math.cos(startLat) * Math.sin(targetLat)
+                - Math.sin(startLat) * Math.cos(targetLat)
+                * Math.cos(diffLong);
         double bearing = Math.atan2(x, y);
-        return (int) Math.round(180 * bearing / Math.PI);
+        double result = Math.toDegrees(bearing);
+        return (int) Math.round(result);
     }
 
     public int getDirectionAzimuth() {
