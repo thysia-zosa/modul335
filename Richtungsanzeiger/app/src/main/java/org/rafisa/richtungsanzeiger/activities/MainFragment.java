fb9ecfbfe8c4913f8310e28f88e3afab2a09b5c6
Jonas Tochtermann <thysia.zosa@gmail.com>
2021-03-04 22:36:47 +0100

    Liste wird jetzt generiert
    und der Kompass erhält die tatsächlichen Koordinaten

--- Richtungsanzeiger/app/src/main/java/org/rafisa/richtungsanzeiger/activities/MainFragment.java
+++ Richtungsanzeiger/app/src/main/java/org/rafisa/richtungsanzeiger/activities/MainFragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -15,6 +17,8 @@ import org.rafisa.richtungsanzeiger.R;
 import org.rafisa.richtungsanzeiger.databinding.FragmentFirstBinding;
 import org.rafisa.richtungsanzeiger.models.Direction;
 import org.rafisa.richtungsanzeiger.models.Location;
+import org.rafisa.richtungsanzeiger.widgets.LocationView;
+import org.rafisa.richtungsanzeiger.widgets.LocationViewListener;
 
 import java.util.ArrayList;
 
@@ -22,6 +26,8 @@ public class MainFragment extends Fragment {
 
     private FragmentFirstBinding binding;
     ArrayList<Location> locationList;
+    private RecyclerView locRecyclerView;
+    private LocationView locationView;
 
     @Override
     public View onCreateView(
@@ -31,16 +37,40 @@ public class MainFragment extends Fragment {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
 
-        locationList = new ArrayList<>();
-        locationList.add(new Location("Matterhorn", 7.6584519, 45.9765738));
-        locationList.add(new Location("Bundeshaus", 7.4442559, 46.9465609));
-
         return binding.getRoot();
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        locationList = new ArrayList<>();
+        locationList.add(new Location("Matterhorn", 7.6584519, 45.9765738));
+        locationList.add(new Location ("Bundeshaus", 7.4442559, 46.9465609));
+
+        LocationViewListener locationViewListener = new LocationViewListener() {
+            @Override
+            public void showLocation(View view, int position) {
+                Location actualLocation = new Location("Hier", 8.5210211, 47.3598043);
+                Location targetLocation = locationList.get(position);
+                Direction direction = new Direction(actualLocation, targetLocation);
+                int directionValue = direction.getDirectionAzimuth();
+                Bundle bundle = new Bundle();
+                bundle.putInt("directionValue", directionValue);
+                NavHostFragment.findNavController(MainFragment.this)
+                        .navigate(R.id.showDirectionAction, bundle);
+            }
+
+            @Override
+            public void editLocation(View view, int position) {
+
+            }
+        };
+
+        locationView = new LocationView(locationList, locationViewListener);
+        locRecyclerView = (RecyclerView) getView().findViewById(R.id.recycler_view);
+        locRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        locRecyclerView.setAdapter(locationView);
 
 //        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -49,24 +79,24 @@ public class MainFragment extends Fragment {
 //                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
 //            }
 //        });
-        binding.fab.setOnClickListener(view1 -> Snackbar.make(view1, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+//        binding.fab.setOnClickListener(view1 -> Snackbar.make(view1, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show());
 
-        binding.showDirectionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Location actualLocation = new Location("Hier", 8.5210211, 47.3598043);
-                Location targetLocation = new Location("Matterhorn", 7.6584519, 45.9765738);
-                Direction direction = new Direction(actualLocation, targetLocation);
-                int directionValue = direction.getDirectionAzimuth();
-                Bundle bundle = new Bundle();
-                bundle.putInt("directionValue", directionValue);
-                NavHostFragment.findNavController(MainFragment.this)
-                        .navigate(R.id.showDirectionAction, bundle);
-            }
-        });
-        binding.editLocationButton.setOnClickListener(v -> NavHostFragment.findNavController(MainFragment.this)
-                .navigate(R.id.action_FirstFragment_to_ThirdFragment));
+//        binding.showDirectionButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Location actualLocation = new Location("Hier", 8.5210211, 47.3598043);
+//                Location targetLocation = new Location("Matterhorn", 7.6584519, 45.9765738);
+//                Direction direction = new Direction(actualLocation, targetLocation);
+//                int directionValue = direction.getDirectionAzimuth();
+//                Bundle bundle = new Bundle();
+//                bundle.putInt("directionValue", directionValue);
+//                NavHostFragment.findNavController(MainFragment.this)
+//                        .navigate(R.id.showDirectionAction, bundle);
+//            }
+//        });
+//        binding.editLocationButton.setOnClickListener(v -> NavHostFragment.findNavController(MainFragment.this)
+//                .navigate(R.id.action_FirstFragment_to_ThirdFragment));
     }
 
     @Override
